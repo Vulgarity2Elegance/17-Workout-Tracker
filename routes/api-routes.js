f23aa0737b2e7e480b6fdfaba0b7d7f11ebd9a65
Youzhi Hu <jyzhu0101@gmail.com>
2020-09-13 22:34:36 +1000

    Created POST request.

---
+++ routes/api-routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const db = require("../models");
+
+const router = express.Router();
+
+router.post("/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
