1801abcf217dd82fd9dada2a3b4187f2ab03bcdd
Youzhi Hu <jyzhu0101@gmail.com>
2020-09-13 22:26:13 +1000

    Setup api-routes in the server.js.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-// Requiring packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 
@@ -14,13 +12,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/custommethods",
-  { useNewUrlParser: true }
-);
-
 const htmlRoutes = require("./routes/html-routes");
 app.use(htmlRoutes);
+const apiRoutes = require("./routes/api-routes");
+app.use(apiRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
