316631301b9ad56a4fa25525cfa07c2b4cf87eed
Youzhi Hu <jyzhu0101@gmail.com>
2020-09-14 14:37:07 +1000

    Deployed on heroku with MongoAtlas setup.

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(apiRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.listen(PORT, () => {
