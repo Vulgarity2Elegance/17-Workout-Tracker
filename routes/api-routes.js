e2373861088db605f678208194bee4c76bf786cd
Youzhi Hu <jyzhu0101@gmail.com>
2020-09-14 13:11:54 +1000

    Completed api-routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,10 +3,49 @@ const db = require("../models");
 
 const router = express.Router();
 
-router.post("/workouts", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push: { exercises: req.body },
+    },
+    { useFindAndModify: false }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//workout range
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .sort({ day: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
     })
     .catch((err) => {
       res.json(err);
